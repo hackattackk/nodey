7a5a2ca877d3f917080c5c92339243de6e916492
hackattackk <67780666+hackattackk@users.noreply.github.com>
2020-07-03 01:17:13 -0700

    Create node.js

---
+++ node.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express()
+
+const port = process.env.PORT || 3000;
+if (typeof port !== "number") {
+  throw Error(`${port} is not a number.`);
+}
+
+app.get("/", (req, res) => res.send(`It is ${new Date()}`));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
